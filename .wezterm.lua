15581af66446f700dcc25471c5398e4669f19c37
Abel Aqui <44988732+TecAbel@users.noreply.github.com>
2026-04-14 17:12:12 -0600

    Enhance WezTerm config with UNIX domain support
    Added UNIX domain support and default GUI startup arguments.

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,11 +14,20 @@ local config = wezterm.config_builder()
 config.font_size = 16
 config.color_scheme = "Gruvbox dark, pale (base16)"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.term = "xterm-256color"
 config.window_decorations = "TITLE | RESIZE" -- NONE
 config.enable_tab_bar = false
+config.underline_thickness = "2pt"
+config.underline_position = "-2pt"
 -- You can specify some parameters to influence the font selection;
 -- for example, this selects a Bold, Italic font variant.
 -- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold", italic = true })
+config.unix_domains = {
+	{
+		name = "unix",
+	},
+}
+config.default_gui_startup_args = { "connect", "unix" }
 
 -- Finally, return the configuration to wezterm:
 return config
